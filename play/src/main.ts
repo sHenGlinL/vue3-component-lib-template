7c2dc2fa17dc3d43a0ad1d3228522b410c82e2cc
liangshenglin <641579182@qq.com>
2022-01-17 16:49:30 +0800

    fix: 修复iconfont打包后乱码的问题

--- play/src/main.ts
+++ play/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue"
 import App from "./app.vue"
 
 import SLIcon from "@sl-plus/components/icon";
-import "@sl-plus/theme-chalk/src/index.scss"
+import "@sl-plus/theme-chalk/dist/index.css"
+// import { SLIcon } from "../../dist/index.esm.js"
 
 const app = createApp(App)
 app.use(SLIcon)
